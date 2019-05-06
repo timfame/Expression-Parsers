37d3a892c8da4a2647e84fb089bc74b1e1ac8fbb
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-05-06 15:15:14 +0300

    Variables modification for Parser Combinators homework added

---
+++ clojure/cljtest/parsing/ClojureVariablesParsingTest.java
@@ -0,0 +1,45 @@
+package cljtest.parsing;
+
+import jstest.ArithmeticTests;
+import jstest.VariablesTests;
+
+import java.util.Random;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public class ClojureVariablesParsingTest extends ClojureObjectParsingTest {
+    private static final Random RANDOM = new Random(4370528470872345L);
+    public ClojureVariablesParsingTest(final boolean hard) {
+        super(new XxxTests(RANDOM), hard);
+    }
+
+    public static void main(final String... args) {
+        new ClojureVariablesParsingTest(mode(args, ClojureVariablesParsingTest.class)).run();
+    }
+
+    static class XxxTests extends VariablesTests {
+        private final Random random;
+
+        public XxxTests(final Random random) {
+            this.random = random;
+            binary("+", (a, b) -> a + b);
+            binary("-", (a, b) -> a - b);
+            binary("*", (a, b) -> a * b);
+            binary("/", (a, b) -> a / b);
+            unary("negate", a -> -a);
+            for (int i = 0; i < 10; i++) {
+                tests.addAll(new ArithmeticTests(var('x', 0), var('y', 1), var('z', 2)).tests);
+            }
+        }
+
+        private AbstractExpression var(final char first, final int i) {
+            final StringBuilder sb = new StringBuilder();
+            sb.append(random.nextBoolean() ? first : Character.toUpperCase(first));
+            while (random.nextBoolean()) {
+                sb.append("xyzXYZ".charAt(random.nextInt(6)));
+            }
+            return variable(sb.toString(), i);
+        }
+    }
+}
